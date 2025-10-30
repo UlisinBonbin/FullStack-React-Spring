5c01bb3bc5f97fcbe82a6287f551651842149530
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-10-30 13:42:21 -0300

    Se subio el proyecto spring a github

---
+++ src/main/java/com/milsabores/backend/PasteleriaMilSaboresApiApplication.java
@@ -0,0 +1,13 @@
+package com.milsabores.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleriaMilSaboresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleriaMilSaboresApiApplication.class, args);
+	}
+
+}
