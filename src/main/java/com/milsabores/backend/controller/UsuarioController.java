ff44966b5a28c7ac1f20441acb4fdaebc08fecc8
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-11-06 22:52:42 -0300

    Se agrego cross origin, ya que al momento de crear otro usuario pedia otro puerto y en cors un nuevo puerto

--- src/main/java/com/milsabores/backend/controller/UsuarioController.java
+++ src/main/java/com/milsabores/backend/controller/UsuarioController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/usuarios")
 @Tag(name = "Usuario Management System")
+@CrossOrigin(origins = "http://localhost:5174")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
